62709c8195fe574dcbdf9477c2e44e12b40a29fb
Wsnusptr <wsnusptr13@gmail.com>
2026-06-06 10:00:57 +0700

    feat: add PmbFaqSection below interactive portal on informasi-pmb page

--- apps/lms/src/app/(student)/informasi-pmb/page.tsx
+++ apps/lms/src/app/(student)/informasi-pmb/page.tsx
@@ -1,12 +1,18 @@
 import PmbInfoPanel from '@/components/student/PmbInfoPanel'
 import PmbInteractivePortal from '@/components/student/PmbInteractivePortal'
+import PmbFaqSection from '@/components/student/PmbFaqSection'
 import AnnouncementFooter from '@/components/layout/AnnouncementFooter'
 
 export default function InformasiPmbPage() {
   return (
     <div className="pb-8">
       <PmbInfoPanel />
-      <PmbInteractivePortal />
+      <div className="mt-6">
+        <PmbInteractivePortal />
+      </div>
+      <div className="mt-6 px-1">
+        <PmbFaqSection />
+      </div>
       <AnnouncementFooter />
     </div>
   )
